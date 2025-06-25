8b27189e192ef4d24e1634c1b654a17a1d88be35
AleHC <alejandro.hernandezcano@epfl.ch>
2025-06-25 19:12:25 +0200

    Moved evaluations from the megatron fork to here. Huge cleanup

---
+++ scripts/update_wandb.py
@@ -0,0 +1,96 @@
+import collections
+import statistics
+import functools
+import re
+import json
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Dict, List, Optional
+
+import wandb
+
+
+def get_log(infos: List[dict], tasks_cfg: dict) -> Dict[str, float]:
+    # Aggregate raw info.
+    groups = {}
+    results = {}
+    all_metrics = set()
+    for info in infos:
+        groups.update(info["group_subtasks"])
+        results.update(info["results"])
+
+    # Prepare final logs.
+    log = collections.defaultdict(dict)
+    for dataname, details in results.items():
+        for metricname, val in details.items():
+            if metricname == "alias" or val == "N/A":
+                continue
+            assert isinstance(val, float), val
+            metricname, _ = metricname.split(",")  # for some reason it is always acc,none so we remove the none.
+            all_metrics.add(metricname)
+            log[dataname][metricname] = val
+
+    # Do macro aggregations.
+    for groupname, subgroups in tasks_cfg.items():
+        missing = set(subgroups) - set(results)
+        if len(missing) > 0:
+            print("WARNING! Macro aggregation for", groupname, "not available. Missing:", sorted(missing))
+            continue
+        metrics = sorted(functools.reduce(set.union, (set(log[dataname]) for dataname in subgroups)))
+        for metric in filter(lambda metric: "stderr" not in metric, all_metrics):
+            values = [log[dataname][metric] for dataname in subgroups if metric in log[dataname]]
+            if len(values) > 0:
+                log[f"{groupname}_macro"][metric] = statistics.mean(values)
+
+    # Finally, push to wandb.
+    wandb_log = {}
+    for dataname, details in log.items():
+        for metric, value in details.items():
+            wandb_log[f"{dataname}/{metric}"] = value
+    return wandb_log
+
+
+def main(logs_root: Path, name: Optional[str], it: Optional[int],
+         tasks: Path):
+
+    with open(tasks) as f:
+        tasks_cfg = json.load(f)
+
+    # Grab each possible log and update wandb run.
+    for p1 in filter(lambda p: name is None or name == p.name, logs_root.iterdir()):
+        print("Updating path", p1)
+        with wandb.init(id=p1.name, name=p1.name) as run:
+            run.define_metric("ConsumedTokens")
+            run.define_metric("*", step_metric="ConsumedTokens")
+
+            for p2 in p1.iterdir():
+                current_it = int(re.match("^iter_([0-9]+)$", p2.name).group(1))
+                if it is not None and it != current_it:
+                    continue
+                print("Updating iteration", current_it)
+
+                with open(p2/"consumed_tokens.txt") as f:
+                    consumed_tokens = int("".join(f).strip())
+                results = []
+                for path in sorted(p2.glob("harness/eval_*/*/results*.json")):
+                    with open(path) as f:
+                        results.append(json.load(f))
+                if len(results) > 0:
+                    log = get_log(results, tasks_cfg)
+                    log.update({"ConsumedTokens": consumed_tokens, "OptStep": current_it})
+                    run.log(log)
+                    sublog = {k: v for k, v in log.items() if "macro/acc" in k}
+                    print("Logged sucessful:", sublog)
+                else:
+                    print("No logs found!")
+                print()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("logs_root", type=Path)
+    parser.add_argument("--name")
+    parser.add_argument("--it", type=int)
+    parser.add_argument("--tasks", type=Path, default=Path("configs/tasks.json"))
+    args = parser.parse_args()
+    main(**vars(args))
