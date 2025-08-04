bfb4bffbf70760bd6c030c2f62ea2204e7862663
AleHC <alejandro.hernandezcano@epfl.ch>
2025-08-04 11:53:00 +0200

    new

--- src/evals/get_info.py
+++ src/evals/get_info.py
@@ -1,12 +1,23 @@
 from evals.tasks import get_all_tasks
 
 
+def get_time_str(minutes: float) -> str:
+    return f"{int(minutes/60)}h{int(minutes) % 60}m{int(minutes*60) % 60}s"
+
+
 if __name__ == "__main__":
+    total_time = 9*60  # Total time for a 70B to run all tasks.
+
     tasks = get_all_tasks()
-    print("Estimated sizes:")
-    print("\n".join(f"{task.name}: {task.size}" for task in sorted(tasks, key=lambda task: task.name)))
     total_size = sum(task.size for task in tasks)
-    print("Total size:", total_size)
+    est = 0
+    print("Estimated sizes:")
+    for task in sorted(tasks, key=lambda task: task.size, reverse=True):
+        estimated_time = total_time*task.size/total_size
+        est += estimated_time
+        print(f"{task.name}: {task.size}rows ({get_time_str(estimated_time)})")
+    print("Total size:", total_size, "Total time:", get_time_str(total_time))
+    print(get_time_str(est))
     print()
 
     dimensions = sorted({task.dimension for task in tasks})
